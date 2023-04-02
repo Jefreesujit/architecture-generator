9e5b928bf7ebfa267dd4fc12c7e01e5171cc0e01
Jefree Sujit <e.jefree110@gmail.com>
2023-04-02 16:42:26 +0000

    changes for frontend library consumption

--- src/dalle2Service.js
+++
@@ -1,9 +0,0 @@
-import { generateImages } from "dalle2";
-
-async function getGeneratedImages(prompt, options = {}) {
-  const { numImages = 1, size = 512, topK = 0, temperature = 0.8 } = options;
-  const result = await generateImages(prompt, { numImages, size, topK, temperature });
-  return result;
-}
-
-export { getGeneratedImages };
