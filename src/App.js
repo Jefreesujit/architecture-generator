1af8c7b076f7227694169cc88d3541cf2e8a1028
Jefree Sujit <e.jefree110@gmail.com>
2023-04-04 06:02:09 +0000

    Udpdate styles and UI for arch gen

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [placeholder, setPlaceholder] = useState("Wall with a photo frame");
   const configuration = new Configuration({
-    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    apiKey: process.env.ARCHGEN_OPENAI_API_KEY,
   });
 
   const openai = new OpenAIApi(configuration);
 
   const generateImage = async () => {
-    setPlaceholder(`Search ${prompt}..`);
+    setPlaceholder(`Search "${prompt}"..`);
     setLoading(true);
     const res = await openai.createImage({
       prompt: prompt,
@@ -24,32 +24,36 @@ function App() {
     setLoading(false);
     setResult(res.data.data[0].url);
   };
+
   return (
-    <div className="app-main">
-      {loading ? (
-        <div className="app-loading">
-          <div className="app-loading-spinner"></div>
-        </div>
-      ) : (
-        <>
-          <h2 className="app-heading">Generate an Image using OpenAI API</h2>
-          <textarea
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-title">ArchGen</h1>
+      </header>
+      <div className="app-main">
+        <div className="app-section">
+          <h2 className="app-section-title">Generate an Image using OpenAI API</h2>
+          <input
+            type="text"
+            id="prompt-input"
             className="app-input"
             placeholder={placeholder}
             onChange={(e) => setPrompt(e.target.value)}
-            rows="10"
-            cols="40"
           />
-          <button className="app-button" onClick={generateImage}>
-            Generate an Image
-          </button>
-          {result.length > 0 ? (
-            <img className="app-result-image" src={result} alt="result" />
-          ) : (
-            <></>
-          )}
-        </>
-      )}
+          <button className="app-button" onClick={generateImage}>Generate an Image</button>
+          <div class="result-section">
+            {loading && (
+              <div className="loader">
+              </div>
+            )}
+            {result.length > 0 ? (
+              <img className="app-result-image" src={result} alt="result" />
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
